468293e6ea4091f8fe62fb77f19fa09e0e2f749f
Teodor Moroz <teomoroz@gmail.com>
2018-06-20 08:44:59 +0300

    Added one to one clasification

--- 03_mnist_classifier.py
+++ 03_mnist_classifier.py
@@ -6,6 +6,10 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 from sklearn.metrics import precision_recall_curve
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_curve
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.preprocessing import StandardScaler
 
 def plotNumber(X):
     digit = X[36000];
@@ -21,6 +25,13 @@ def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
     plt.legend(loc="center left")
     plt.ylim([0, 1])
 
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
 if __name__ == "__main__":
     mnist = fetch_mldata("MNIST original")
     X, y = mnist["data"], mnist["target"]
@@ -35,6 +46,9 @@ if __name__ == "__main__":
 
     y_train_5 = (y_train == 5)
     X_train_5 = (X_train == 5)
+
+    some_digit = X[36000]
+    some_digit_image = some_digit.reshape(28, 28)
 
     sgd_classifier = SGDClassifier(random_state=42)
     sgd_classifier.fit(X_train, y_train_5)
@@ -56,4 +70,34 @@ if __name__ == "__main__":
                                  method="decision_function")
     precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
     plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+    plt.show()
+
+    fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+    forest_clf = RandomForestClassifier(random_state=42)
+    y_probas_forest = cross_val_predict(forest_clf, X_train, y_train_5, cv=3, method="predict_proba")
+    y_scores_forest = y_probas_forest[:, 1]
+    fpr_forest, tpr_forest, thresholds_forest = roc_curve(y_train_5, y_scores_forest)
+
+    plt.plot(fpr, tpr, "b:", label="SGD")
+    plot_roc_curve(fpr_forest, tpr_forest, "Random Forest")
+    plt.legend(loc="lower right")
+    plt.show()
+
+    ovo_clf = OneVsOneClassifier(SGDClassifier(random_state=42))
+    ovo_clf.fit(X_train, y_train)
+    ovo_clf.predict([some_digit])
+
+    sgd_clf = SGDClassifier(random_state=42)
+    sgd_clf.fit(X_train, y_train_5)
+
+
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train.astype(np.float64))
+    cross_val_score(sgd_clf, X_train_scaled, y_train, cv=3, scoring="accuracy")
+    y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+
+#create confusion matrix
+    conf_matrix = confusion_matrix(y_train, y_train_pred)
+    print (conf_matrix)
+    plt.matshow(conf_matrix, cmap=plt.cm.gray)
     plt.show()
