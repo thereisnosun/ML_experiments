3f4fbd721de50c9da7ac1a31cdbedacaf2075f4b
Teodor Moroz <teomoroz@gmail.com>
2018-12-18 08:29:41 +0200

    Reinforcement train algo

--- 16_reinforcement.py
+++ 16_reinforcement.py
@@ -1,4 +1,6 @@
 import gym
+import tensorflow as tf
+import numpy as np
 
 env = gym.make("CartPole-v0")
 obs = env.reset()
@@ -23,7 +25,23 @@ for episode in range(500):
     #env.render(mode="rgb_array")
 
 
-import tensorflow as tf
+def discount_rewards(rewards, discount_rate):
+    discounted_rewards = np.empty(len(rewards))
+    cumulative_rewards = 0
+    for step in reversed(range(len(rewards))):
+        cumulative_rewards = rewards[step] + cumulative_rewards * discount_rate
+        discounted_rewards[step] = cumulative_rewards
+    return discounted_rewards
+
+def discount_and_normalize_rewards(all_rewards, discount_rate):
+    all_discounted_rewards = [discount_rewards(rewards, discount_rate)
+                        for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [(discounted_rewards - reward_mean)/reward_std
+        for discounted_rewards in all_discounted_rewards]
+
 
 n_inputs, n_hiden = 4, 4
 n_outputs = 1
@@ -31,10 +49,71 @@ initializer = tf.contrib.layers.variance_scaling_initializer()
 
 X = tf.placeholder(tf.float32, shape=[None, n_inputs])
 hidden = tf.layers.dense(X, n_hiden, activation=tf.nn.elu, kernel_initializer=initializer)
-logits = tf.layers.dense(hidden, n_inputs, kernel_initializer=initializer)
+logits = tf.layers.dense(hidden, n_outputs, kernel_initializer=initializer)
 outputs = tf.nn.sigmoid(logits)
 
 p_left_and_right = tf.concat(axis=1, values=[outputs, 1-outputs])
 action = tf.multinomial(tf.log(p_left_and_right), num_samples=1)
 
 init = tf.global_variables_initializer()
+
+y = 1. - tf.to_float(action)
+learning_rate = 0.01
+cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y,
+                                                        logits=logits)
+optimizer = tf.train.AdamOptimizer(learning_rate)
+grads_and_vars = optimizer.compute_gradients(cross_entropy)
+gradients = [grad for grad, variable in grads_and_vars]
+
+gradient_placeholders = []
+grads_and_vars_feed = []
+for grad, variable in grads_and_vars:
+    gradient_placeholder = tf.placeholder(tf.float32, shape=grad.get_shape())
+    gradient_placeholders.append(gradient_placeholder)
+    grads_and_vars_feed.append((gradient_placeholder, variable))
+
+training_op = optimizer.apply_gradients(grads_and_vars_feed)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+n_iterations = 250
+n_max_steps = 1000
+n_games_per_update = 10
+save_iterations = 10
+discount_rate = 0.95
+
+with tf.Session() as sess:
+    init.run()
+    for iteration in range(n_iterations):
+        all_rewards = []
+        all_gradients = []
+        for game in range(n_games_per_update):
+            current_rewards = []
+            current_gradients = []
+            obs = env.reset()
+            for step in range(n_max_steps):
+                action_val, gradients_val = sess.run(
+                    [action, gradients],
+                    feed_dict={X: obs.reshape(1, n_inputs)})
+                obs, reward, done, info = env.step(action_val[0][0])
+                current_rewards.append(reward)
+                current_gradients.append(gradients_val)
+                if done:
+                    break
+                all_rewards.append(current_rewards)
+                all_gradients.append(current_gradients)
+
+            all_rewards = discount_and_normalize_rewards(all_rewards, discount_rate)
+            feed_dict = {}
+            for var_index, grad_placeholder in enumerate(gradient_placeholders):
+                mean_gradients = np.mean(
+                    [reward * all_gradients[game_index][step][var_index]
+                        for game_index, rewards in enumerate(all_rewards)
+                        for step, reward in enumerate(rewards)],
+                    axis=0)
+                feed_dict[grad_placeholder] = mean_gradients
+            sess.run(training_op, feed_dict=feed_dict)
+            if iteration % save_iterations == 0:
+                saver.save(sess, "./policy_net_pkg.pkt")
+
